7b9192e34f0873ba9f2263f68bc5725e4cacc714
fyui001 <yun.cod@gmail.com>
2025-04-14 12:32:31 +0900

    Replace database migrations & Add model

---
+++ src/Sophia.Api/Migrations/20250414033055_InitialCreate.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sophia.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "admin_user",
+                columns: table => new
+                {
+                    id = table.Column<ulong>(type: "bigint unsigned", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    emai = table.Column<string>(type: "varchar(255)", nullable: true, comment: "メールアドレス")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    name = table.Column<string>(type: "varchar(255)", nullable: false, comment: "名前")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    role = table.Column<string>(type: "varchar(255)", nullable: false, comment: "ロール")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    status = table.Column<string>(type: "varchar(255)", nullable: false, comment: "ステータス")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false, comment: "作成日時"),
+                    updated_at = table.Column<DateTime>(type: "datetime(6)", nullable: false, comment: "更新日時")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_admin_user", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "user_definitive_register_token",
+                columns: table => new
+                {
+                    id = table.Column<ulong>(type: "bigint unsigned", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    user_id = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    token = table.Column<string>(type: "longtext", nullable: false, comment: "トークン")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    is_verified = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    expired_at = table.Column<DateTime>(type: "datetime(6)", nullable: false, comment: "有効期限"),
+                    created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false, comment: "作成日時"),
+                    updated_at = table.Column<DateTime>(type: "datetime(6)", nullable: false, comment: "更新日時")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_definitive_register_token", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<ulong>(type: "bigint unsigned", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    emai = table.Column<string>(type: "varchar(255)", nullable: true, comment: "メールアドレス")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    name = table.Column<string>(type: "varchar(255)", nullable: false, comment: "名前")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    icon_url = table.Column<string>(type: "varchar(255)", nullable: false, comment: "アイコンURL")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    status = table.Column<string>(type: "varchar(255)", nullable: false, comment: "ステータス")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    created_at = table.Column<DateTime>(type: "datetime(6)", nullable: false, comment: "作成日時"),
+                    updated_at = table.Column<DateTime>(type: "datetime(6)", nullable: false, comment: "更新日時")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_definitive_register_token_user_id",
+                table: "user_definitive_register_token",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_emai",
+                table: "users",
+                column: "emai",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "admin_user");
+
+            migrationBuilder.DropTable(
+                name: "user_definitive_register_token");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
